cb891d1c995e8186fe0345e88f21fdbb45ac3f09
Joanantha Matheus <jmvg.comp@gmail.com>
2021-07-24 17:37:34 -0300

    SplashActivity - Código | Parcial

--- app/src/main/java/dev/jmvg/frasesmotivacionais/SplashActivity.kt
+++ app/src/main/java/dev/jmvg/frasesmotivacionais/SplashActivity.kt
@@ -1,11 +1,35 @@
 package dev.jmvg.frasesmotivacionais
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity(), View.OnClickListener {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_splash)
+
+    val botaoSalvar = findViewById<Button>(R.id.botaoSalvar);
+    botaoSalvar.setOnClickListener(this)
+
+
+  }
+
+  override fun onClick(view: View?) {
+    val id = view?.id
+    if(id == R.id.botaoSalvar) handleSave();
+  }
+
+  private fun handleSave() {
+    val name = findViewById<EditText>(R.id.editNomeUsuario).text.toString()
+    if(name.isNotBlank()){
+      startActivity(Intent(this, MainActivity::class.java))
+    }else{
+      Toast.makeText(this, "O nome não pode estar vazio :(", Toast.LENGTH_LONG).show()
+    }
   }
 }
