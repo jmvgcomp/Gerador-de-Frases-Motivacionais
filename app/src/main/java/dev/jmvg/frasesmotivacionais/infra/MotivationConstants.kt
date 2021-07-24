5a83d3f8705e80a653ec6c7032651f754fbcc951
Joanantha Matheus <jmvg.comp@gmail.com>
2021-07-24 19:38:58 -0300

    Implementação do SharedPreferences, passando valores para outra activity | Parcial

---
+++ app/src/main/java/dev/jmvg/frasesmotivacionais/infra/MotivationConstants.kt
@@ -0,0 +1,7 @@
+package dev.jmvg.frasesmotivacionais.infra
+
+class MotivationConstants private constructor(){
+  object KEY {
+    val PERSON_NAME: String = "name"
+  }
+}
--- app/src/main/java/dev/jmvg/frasesmotivacionais/ui/MainActivity.kt
+++ app/src/main/java/dev/jmvg/frasesmotivacionais/ui/MainActivity.kt
@@ -2,11 +2,24 @@ package dev.jmvg.frasesmotivacionais.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import dev.jmvg.frasesmotivacionais.R
+import dev.jmvg.frasesmotivacionais.infra.MotivationConstants
+import dev.jmvg.frasesmotivacionais.infra.SecurityPreferences
 
 class MainActivity : AppCompatActivity() {
+
+  private lateinit var mSecurityPreferences: SecurityPreferences
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+
+    val textName = findViewById<TextView>(R.id.editText)
+
+    mSecurityPreferences = SecurityPreferences(this)
+    textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+
   }
 }
