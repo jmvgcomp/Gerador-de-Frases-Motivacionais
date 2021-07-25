88f5e3d7778ad65992f1ed72b9936bbefb527003
Joanantha Matheus <jmvg.comp@gmail.com>
2021-07-24 21:42:34 -0300

    Referenciando as imageViews na MainActivity - codigo | Parcial

--- app/src/main/java/dev/jmvg/frasesmotivacionais/infra/MotivationConstants.kt
+++ app/src/main/java/dev/jmvg/frasesmotivacionais/infra/MotivationConstants.kt
@@ -4,4 +4,11 @@ class MotivationConstants private constructor(){
   object KEY {
     val PERSON_NAME: String = "name"
   }
+
+  object PHRASEFILTER{
+    val ALL = 1
+    val HAPPY = 2
+    val MORNING = 3
+  }
+
 }
--- app/src/main/java/dev/jmvg/frasesmotivacionais/ui/MainActivity.kt
+++ app/src/main/java/dev/jmvg/frasesmotivacionais/ui/MainActivity.kt
@@ -2,12 +2,15 @@ package dev.jmvg.frasesmotivacionais.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 import dev.jmvg.frasesmotivacionais.R
 import dev.jmvg.frasesmotivacionais.infra.MotivationConstants
 import dev.jmvg.frasesmotivacionais.infra.SecurityPreferences
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
   private lateinit var mSecurityPreferences: SecurityPreferences
 
@@ -19,7 +22,55 @@ class MainActivity : AppCompatActivity() {
     val textName = findViewById<TextView>(R.id.textName)
 
     mSecurityPreferences = SecurityPreferences(this)
-    textName.text = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+    val userName = mSecurityPreferences.getString(MotivationConstants.KEY.PERSON_NAME)
+    textName.text = "Olá, $userName"
+
+    findViewById<ImageView>(R.id.imageAll).setOnClickListener(this)
+    findViewById<ImageView>(R.id.imageHappy).setOnClickListener(this)
+    findViewById<ImageView>(R.id.imageMorning).setOnClickListener(this)
 
   }
+
+  override fun onClick(view: View) {
+    val id = view.id;
+    val listFilter = listOf(R.id.imageAll, R.id.imageHappy, R.id.imageMorning)
+
+    if(id == R.id.buttonNewPhrase){
+      handleNewPhrase()
+    }else if(id in listFilter){
+      handleFilter(id)
+    }
+
+  }
+
+  private fun handleFilter(id: Int) {
+
+    val imageAll = findViewById<ImageView>(R.id.imageAll)
+    val imageHappy = findViewById<ImageView>(R.id.imageHappy)
+    val imageMorning = findViewById<ImageView>(R.id.imageMorning)
+
+    imageAll.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+    imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+    imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.colorAccent))
+
+      when(id){
+        R.id.imageAll -> {
+          imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        }
+        R.id.imageHappy -> {
+          imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        }
+        R.id.imageMorning -> {
+          imageMorning.setColorFilter(ContextCompat.getColor(this, R.color.white))
+        }
+      }
+  }
+
+  private fun handleNewPhrase() {
+
+  }
+
+
+
+
 }
